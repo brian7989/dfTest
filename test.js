cb73a4b62d3cfe8f490536071c20da20a2831ffb
Brian Taesung Lee <brian.ts.lee.0907@gmail.com>
2021-06-20 00:56:17 +0900

    change \\n error

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ const port = process.env.PORT                               // Port
 const options = {                                           // Credentials
     credentials: {
         client_email: process.env.CLIENT_EMAIL,
-        private_key: process.env.PRIVATE_KEY
+        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
     }
 };
 
