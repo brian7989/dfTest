a15524d18e74031c3a69eb9c16c8a33f053bfe90
Brian Taesung Lee <brian.ts.lee.0907@gmail.com>
2021-06-20 03:03:49 +0900

    rename vars

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
 // IMPORTS
-// require('dotenv').config()
+// require('dotenv').config()                               // Only for local testing
 const dialogflow = require('@google-cloud/dialogflow');     // Imports the Dialogflow library
 const express = require('express')                          // Imports the NodeJS web applicaion framework 
 
 // ENVIRONMENT SETUP
 const projectId = process.env.PROJECTID;                    // ProjectID
-const sessionId = Math.random().toString(36).substring(7);  // Random sessionId
-let query = "HELLO";                                      // Query. To be changed to user's input
+const sessionId = Math.random().toString(36).substring(7);  // Random sessionId for client
 const languageCode = 'en';                                  // Language code in English
 const port = process.env.PORT                               // Port
 const options = {                                           // Credentials
@@ -17,20 +16,18 @@ const options = {                                           // Credentials
 };
 
 // Initialize Express App
-const app = express()
+const expressApp = express()
 
 // Listen on specified port
-app.listen(port, () => {
+expressApp.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
 })
 
-// Response format
-
 // Send response on base URL
-app.get('/', (req, res) => {
-    query = req.query.q
-    console.log(projectId, sessionId, port, options);
-    executeQuery(projectId, sessionId, query, languageCode).then(function(resp) {
+expressApp.get('/', (req, res) => {
+    // Request parameter q  -->  .../?q
+    const query = req.query.q
+    sendAPIquery(projectId, sessionId, query, languageCode).then(function(resp) {
         console.log("Successful response: ", resp);
         const response = {
             resp: resp,
@@ -39,23 +36,24 @@ app.get('/', (req, res) => {
     })
 })
 
-// Instantiates a session client
+// Create session client
 const sessionClient = new dialogflow.SessionsClient(options);
 
-async function detectIntent(
+// Function that gets the intent of query
+async function getIntent(
     projectId,
     sessionId,
     query,
     contexts,
     languageCode
 ) {
-    // The path to identify the agent that owns the created intent.
+    // The path to identify the agent that owns the created intent
     const sessionPath = sessionClient.projectAgentSessionPath(
         projectId,
         sessionId
     );
 
-    // The text query request.
+    // The text query request
     const request = {
         session: sessionPath,
         queryInput: {
@@ -72,30 +70,32 @@ async function detectIntent(
         };
     }
 
-    const responses = await sessionClient.detectIntent(request);
+    const responses = await sessionClient.getIntent(request);
     return responses[0];
 }
 
-async function executeQuery(projectId, sessionId, query, languageCode) {
-    // Keeping the context across queries let's us simulate an ongoing conversation with the bot
+// Sends off query to the API
+async function sendAPIquery(projectId, sessionId, query, languageCode) {
+    // Context to continue conversation with chatbot
     let context;
-    let intentResponse;
+    let intent;
     try {
-        console.log(`Sending Query: ${query}`);
-        intentResponse = await detectIntent(
+        console.log(`Sending query: ${query}`);
+        // First, get intent
+        intent = await getIntent(
             projectId,
             sessionId,
             query,
             context,
             languageCode
         );
-        console.log('Detected intent');
+        console.log('Intent Detected');
         // Use the context from this response for next queries, if required
-        // context = intentResponse.queryResult.outputContexts;
+        context = intent.queryResult.outputContexts;
 
-        // Fulfillment text from DialogFlow API
-        let response = `${intentResponse.queryResult.fulfillmentText}`
-        console.log(response);
+        // Fulfillment text from our DialogFlow API
+        let response = `${intent.queryResult.fulfillmentText}`
+        console.log("Fulfillment Text:", response);
         return response
     } catch (error) {
         console.log(error);
